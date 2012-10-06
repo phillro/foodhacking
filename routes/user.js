0e8a75ddaf33f58bdbecf4993cbfc45b46e69a45
Jason Teplitz <Jason@Jason-Teplitzs-MacBook-Pro.local>
2012-10-06 13:40:34 -0400

    fixed multiple user saving bug

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 (function(){
   "use strict";
   var foursquare = require("./Foursquare.js"),
+      _          = require("underscore"),
       async      = require("async");
 
-  var saveUser;
+  var getUser;
 
   exports.login = function(req, res, next){
     res.redirect(foursquare.getAuthRedirect());
@@ -11,11 +12,12 @@
 
   exports.foursquareCallback = function(req, res, next){
     var oauth_token, schemas = req.models;
-    async.waterfall([
+    var waterfall_arr = [
       function(cb){ foursquare.getAccessToken(req.query.code, cb) },
       function(token, cb){ oauth_token = token; foursquare.getUserInfo(token, cb) },
-      function(user, cb) { saveUser(schemas, oauth_token, user, cb) }
-    ], function(err, user){
+      function(user, cb) { getUser(schemas, oauth_token, user, cb) }
+    ];
+    async.waterfall(waterfall_arr, function(err, user){
       if (err){
         console.error(err);
         return next(500);
@@ -26,13 +28,19 @@
     });
   };
 
-  saveUser = function(schemas, token, fsqUser, cb){
-    var user = new schemas.User({
-      foursquare_token: token,
-      foursquare_id: fsqUser.user.id,
-      first_name: fsqUser.user.firstName,
-      last_name: fsqUser.user.lastName
+  getUser = function(schemas, token, fsqUser, cb){
+    schemas.User.findOne({foursquare_id: fsqUser.user.id}, function(err, user){
+      if (_.isNull(user)){
+        user = new schemas.User({
+          foursquare_token: token,
+          foursquare_id: fsqUser.user.id,
+          first_name: fsqUser.user.firstName,
+          last_name: fsqUser.user.lastName
+        });
+        user.save(cb);
+      } else {
+        cb(false, user);
+      }
     });
-    user.save(cb);
   };
 }());
