aef84ac1bf595c56bfbeb627721e73c73a022053
Jason Teplitz <Jason@Jason-Teplitzs-MacBook-Pro.local>
2012-10-06 15:27:55 -0400

    working on home page restaurants list

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,8 +2,30 @@
   "use strict";
 
   // functions
+  var getRestaurants, showRestaurants;
 
   $(document).ready(function(){
+    getRestaurants();
   });
+
+  getRestaurants = function(){
+    var loc = $.cookie("geolocation").split(";");
+    var lat = loc[0], lng = loc[1];
+
+    $.ajax({
+      url: "/restaurants/" + lat + "," + lng,
+      success: showRestaurants,
+      failure: function(err){
+        console.log(err);
+        alert("Can't load restaurants");
+      }
+    });
+  };
+
+  showRestaurants = function(data){
+    data = JSON.parse(data);
+
+    console.log(data[0]);
+  };
 
 }());
