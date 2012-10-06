7b56223e1f626b4f5ce1712b608b47d04a607f90
Luis E Fabian <hello@luisefabian.com>
2012-10-06 11:57:11 -0400

    HBS

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var elasticSearchCilent = new ElasticSearchClient(nconf.get('elasticsearch'));
 // Configuration
 app.configure(function () {
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'hbs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
